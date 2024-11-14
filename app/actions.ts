c908eec259b0fa2d5ca00b234f365a5259c96c7a
JeongbinYoon <yon980824@gmail.com>
2024-11-15 02:16:28 +0900

    feat: todo 목록 조회, react-query 상태 관리

--- app/actions.ts
+++ app/actions.ts
@@ -4,6 +4,10 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+export const fetchTasks = async () => {
+  return await prisma.todo.findMany();
+};
+
 export const createTask = async (title: string) => {
   return await prisma.todo.create({
     data: { title, isDone: false },
--- app/components/Tasks.tsx
+++ app/components/Tasks.tsx
@@ -1,23 +1,16 @@
 'use client';
 
-import { useAtom } from 'jotai';
 import Task from './Task';
-import { filterAtom, tasksAtom } from '@/store/taskAtom';
+import { fetchTasks } from '@/app/actions';
+import { useQuery } from 'react-query';
 
 const Tasks = () => {
-  const [tasks] = useAtom(tasksAtom);
-  const [filterVal] = useAtom(filterAtom);
-
-  // 필터링
-  const filteredTasks = tasks.filter(
-    (task) => filterVal === 'all' || task.isDone === (filterVal === 'complete')
-  );
-
+  const { data: tasks, error, isLoading } = useQuery('tasks', fetchTasks);
+  if (error) return <p>error..........</p>;
+  if (isLoading) return <p>Loading..</p>;
   return (
     <ul className='max-h-96 overflow-scroll'>
-      {filteredTasks.map((task) => (
-        <Task key={task.id} task={task} />
-      ))}
+      {tasks && tasks.map((task) => <Task key={task.id} task={task} />)}
     </ul>
   );
 };
