c1b9834e0c9a9531fbd4a436cd71f5272c3b3b8f
JeongbinYoon <yon980824@gmail.com>
2024-11-24 00:15:58 +0900

    feat: useCalendar 훅 월 변경 날짜 계산 기능 추가

--- hooks/useCalendar.ts
+++ hooks/useCalendar.ts
@@ -6,25 +6,25 @@ import {
   format,
   startOfMonth,
   startOfWeek,
+  addMonths,
+  getMonth,
 } from 'date-fns';
+import { useState, useEffect, useCallback } from 'react';
+
 const useCalendar = () => {
+  const TOTAL_WEEK_DAYS = 7;
   const today = new Date();
+
+  const [currentDate, setCurrentDate] = useState(today);
+  const currentMonth = getMonth(currentDate) + 1;
+
+  // 요일 계산
   const weekStartDate = startOfWeek(today);
-  const daysOfWeek = Array.from({ length: 7 }).map((_, i) =>
-    format(addDays(weekStartDate, i), 'EEEEE')
+  const daysOfWeek = Array.from({ length: TOTAL_WEEK_DAYS }).map((_, i) =>
+    format(addDays(weekStartDate, i), 'EEE')
   );
 
-  const startDateOfMonth = startOfMonth(today);
-  const endDateOfMonth = endOfMonth(startDateOfMonth);
-
-  const days = eachDayOfInterval({
-    start: startOfWeek(startDateOfMonth),
-    end: endOfWeek(endDateOfMonth),
-  }).map((date) => date.getDate());
-
-  /*
-   * 인자로 받은 배열 사이즈에 맞춰 중첩 배열로 만들기
-   */
+  // 인자로 받은 배열 사이즈에 맞춰 중첩 배열로 만들기
   const chunkArray = <T>(arr: T[], size: number): T[][] => {
     const result: T[][] = [];
     for (let i = 0; i < arr.length; i += size) {
@@ -32,11 +32,41 @@ const useCalendar = () => {
     }
     return result;
   };
-  const calendarDates = chunkArray(days, 7);
+
+  // 인자로 받은 날짜 기준 해당 월 날짜 범위 계산 함수
+  const calculateCalendarDates = useCallback((date: Date) => {
+    const startDateOfMonth = startOfMonth(date);
+    const endDateOfMonth = endOfMonth(startDateOfMonth);
+
+    const days = eachDayOfInterval({
+      start: startOfWeek(startDateOfMonth),
+      end: endOfWeek(endDateOfMonth),
+    }).map((day) => day.getDate());
+
+    return chunkArray(days, TOTAL_WEEK_DAYS);
+  }, []);
+
+  // 달력 날짜 상태
+  const [calendarDates, setCalendarDates] = useState(
+    calculateCalendarDates(currentDate)
+  );
+
+  // 달력을 업데이트하는 함수
+  const setCurrentMonth = (monthOffset: number) => {
+    const newDate = addMonths(currentDate, monthOffset);
+    setCurrentDate(newDate);
+  };
+
+  // currentDate 변경 시 calendarDates 동기화
+  useEffect(() => {
+    setCalendarDates(calculateCalendarDates(currentDate));
+  }, [currentDate, calculateCalendarDates]);
 
   return {
     daysOfWeek,
     calendarDates,
+    currentMonth,
+    setCurrentMonth,
   };
 };
 
